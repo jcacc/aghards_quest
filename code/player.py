4a8809a4a184ea91a633e21f0a52dcde12e80246
jcacc <joe@accardi.xyz>
2023-03-19 19:35:43 -0400

    player movement

--- code/player.py
+++ code/player.py
@@ -3,6 +3,29 @@ from settings import *
 
 class Player(pygame.sprite.Sprite):
     def __init__(self,pos,groups):
-        super().__init__()
-        self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
+        super().__init__(groups)
+        self.image = pygame.image.load("C:\\python_projects\\aghards_quest\\graphics\\test\\player.png").convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
+
+        self.direction = pygame.math.Vector2()
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction = 0
+            
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction = 0
+
+    
+    def update(self):
+        self.input()
