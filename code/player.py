480a6a29bf46677185c68dc5dbcd0c1218badc25
jcacc <joe@accardi.xyz>
2023-03-20 20:38:57 -0400

    fixed the movement crash with the help of chatgpt :)

--- code/player.py
+++ code/player.py
@@ -7,11 +7,11 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load("C:\\python_projects\\aghards_quest\\graphics\\test\\player.png").convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
 
-        self.direction = pygame.math.Vector2()
+        
 
     def input(self):
         keys = pygame.key.get_pressed()
-
+        self.direction = pygame.math.Vector2(0,0)
         if keys[pygame.K_UP]:
             self.direction.y = -1
         elif keys[pygame.K_DOWN]:
@@ -27,5 +27,6 @@ class Player(pygame.sprite.Sprite):
             self.direction = 0
 
     
-    def update(self):
-        self.input()
+def update(self):
+    self.input()
+    self.rect.move_ip(self.direction.x * PLAYER_SPEED, self.direction.y * PLAYER_SPEED)
